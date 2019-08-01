a9b2fbfc66024cfb5b844d843fda1cee407dce28
Peter Adeoye <cwizard2011@gmail.com>
2019-08-01 19:08:09 +0100

    initial commit

---
+++ src/TuringBackend.Api/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using TuringBackend.Api.Core;
+
+namespace TuringBackend.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseBeatPulse(options =>
+                {
+                    options.ConfigurePath("health")
+                        .ConfigureTimeout(1500) // default -1 infinitely
+                        .ConfigureDetailedOutput(true, true); //default (true,false)
+                })
+                .UseStartup<Startup>()
+                .UseLogging();
+        }
+    }
+}
